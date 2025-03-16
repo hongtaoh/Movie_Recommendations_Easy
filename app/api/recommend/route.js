ed7a22923f157d5760063d6edbf9f51f3f119f1b
hongtaoh <hongtaoh@cs.wisc.edu>
2025-03-15 21:08:23 -0500

    fix typo

--- app/api/recommend/route.js
+++ app/api/recommend/route.js
@@ -17,11 +17,8 @@ export async function POST(req) {
         }
 
         const embedding = await getGeminiEmbedding(query);
-        console.log(embedding)
         const qdrantResults = await qdrantSearch(embedding, 5);
-        console.log(qdrantResults)
         const explanation = await generateExplanation(query, qdrantResults, model)
-        console.log(explanation)
 
         return NextResponse.json({ movies: qdrantResults, explanation})
     } catch (error) {
